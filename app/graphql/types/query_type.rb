3a94cd4b058101446c2f355805090bb3fdc8678b
Guilherme Silva <guilherme.gss@outlook.com.br>
2024-03-22 11:54:02 -0300

    update arquivos graphql

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,21 @@
 module Types
-  class QueryType < GraphQL::Schema::Object
+  class QueryType < BaseObject
+    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
+      argument :id, ID, required: true, description: "ID of the object."
+    end
+
+    def node(id:)
+      context.schema.object_from_id(id, context)
+    end
+
+    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
+      argument :ids, [ID], required: true, description: "IDs of the objects."
+    end
+
+    def nodes(ids:)
+      ids.map { |id| context.schema.object_from_id(id, context) }
+    end
+
     description "The query root of this schema"
 
     field :policy, resolver: Resolvers::PolicyResolver
