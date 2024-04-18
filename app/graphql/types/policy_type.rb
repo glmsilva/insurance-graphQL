9e2d3425cc053be01a61320e8c528b46a0ca9217
Guilherme Silva <guilherme.gss@outlook.com.br>
2024-04-18 16:58:17 -0300

    atualiza policy_id no type

--- app/graphql/types/policy_type.rb
+++ app/graphql/types/policy_type.rb
@@ -2,14 +2,14 @@ module Types
   class PolicyType < Types::BaseObject
     description "a policy type"
 
-    field :policy_id, String, null: false
+    field :policy_id, ID, null: false
     field :effective_date, String, null: false, description: "the specific day when the insurance coverage begins"
     field :expiration_date, String, null: false, description: "the day the insurance coverage ends"
     field :insured_person, Types::InsuredPersonType, null: false, description: "person who will receive the benefit under the insurance policy after the occurrence loss"
     field :vehicle, Types::VehicleType, null: false, description: "the good of the insured"
 
     def policy_id
-      object["id"].to_s
+      object["id"]
     end
   end
 end
